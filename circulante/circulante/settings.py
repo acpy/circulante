f78a267107561d2bc2967d481e67c9daab289358
Luciano Ramalho <luciano@ramalho.org>
2012-04-17 22:15:30 -0300

    settings locais separados do settings.py

--- circulante/circulante/settings.py
+++ circulante/circulante/settings.py
@@ -27,11 +27,11 @@ DATABASES = {
 # timezone as the operating system.
 # If running in a Windows environment this must be set to the same as your
 # system time zone.
-TIME_ZONE = 'America/Chicago'
+TIME_ZONE = 'America/Sao_Paulo'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
 SITE_ID = 1
 
@@ -119,9 +119,9 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+    'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
-    # 'django.contrib.admindocs',
+    'django.contrib.admindocs',
 )
 
 # A sample logging configuration. The only tangible logging
@@ -152,3 +152,15 @@ LOGGING = {
         },
     }
 }
+
+import os
+PROJECT_PATH = os.path.abspath(os.path.dirname(__file__))
+execfile(os.path.join(PROJECT_PATH,'settings_local.include'))
+
+
+
+
+
+
+
+
