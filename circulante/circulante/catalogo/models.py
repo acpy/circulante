834b1cb461a687089d3b00e8f67df9dfaf3c1f01
Luciano Ramalho <luciano@ramalho.org>
2012-04-19 20:29:23 -0300

    modelo de credito e massa de dados em formato texto

--- circulante/circulante/catalogo/models.py
+++ circulante/circulante/catalogo/models.py
@@ -13,4 +13,11 @@ class Publicacao(models.Model):
                             default=TIPOS_PUBLICACAO[0][0])
     id_padrao = models.CharField(max_length=32, blank=True)
     titulo = models.CharField(max_length=256)
+    num_paginas = models.PositiveIntegerField(default=0)
+
+class Credito(models.Model):
+    nome = models.CharField(max_length=256)
+    papel = models.CharField(max_length=32, blank=True)
+    publicacao = models.ForeignKey(Publicacao)
+
     
