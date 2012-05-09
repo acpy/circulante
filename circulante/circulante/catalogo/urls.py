5acb067a6a7d7ff526bb0d87ef7c09ea8715a2e0
Luciano Ramalho <luciano@ramalho.org>
2012-05-08 21:58:13 -0300

    configuracoes para rodar com o Apache 2

--- circulante/circulante/catalogo/urls.py
+++ circulante/circulante/catalogo/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import patterns, include, url
 from .views import busca, catalogar, editar
 
 urlpatterns = patterns('',
-    url(r'busca', busca, name='busca'),
     url(r'catalogar', catalogar, name='catalogar'),
     url(r'editar/(\d+)', editar, name='editar'),
+    url(r'', busca, name='busca'),
 )
